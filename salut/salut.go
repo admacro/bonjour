cfc9abee029b4be49c1c6aaa6e6dd3e3757cb026
James Ni <admacro@gmail.com>
2020-04-13 12:20:53 +0800

    remove side effect

--- salut/salut.go
+++ salut/salut.go
@@ -1,8 +1,5 @@
 package salut
 
-// side effect of improting salut with the blank identifier as explicit package name
-var RandomNumber = 123456
-
 func Salut(gender int) string {
 	switch gender {
 	case 1:
